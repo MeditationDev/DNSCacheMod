7e1687b55b70bd3ad72a53c9bdf360c8d8ed6fc7
liulihaocai <liulihaocai@outlook.com>
2020-10-21 22:49:07 +0800

    v1.1

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Support all versions of Minecraft theoretically
 Command is */dnscachemod*  
 Usages:  
 */dnscachemod add from target* ADD A DOMAIN FOR DNSCACHE  
-*/dnscachemod remove* from REMOVE A DOMAIN FOR DNSCACHE  
+*/dnscachemod remove from* REMOVE A DOMAIN FOR DNSCACHE  
+*/dnscachemod createsharelink from target* CREATE A DNSCACHE SHARE LINK  
+*/dnscachemod loadsharelink link* LOAD A DNSCACHE SHARE LINK  
 */dnscachemod list* LIST DOMAINS USING DNSCACHE  
 */dnscachemod reload* RELOAD DNSCACHES  
 
@@ -18,6 +20,12 @@ format:
     {"from":"mc.hypixel.net","target":"meditationdev.org"}
 ]
 ~~~
-and connect to "meditationdev.org" in minecraft then go into "mc.hypixel.net"  
+and connect to "meditationdev.org" in minecraft then go into ""  
 *from* is the original server address  
 *target* is the target you want to change DNS for it  
+
+## ShareLink
+It is a base64 Str begin with *dnscache://* and split with "!"  
+like this:*dnscache://aWRjLmN1YmVwYWFzLmNvbSFhY2NlbC5oeXBpeGVsLm5ldA==*  
+"aWRjLmN1YmVwYWFzLmNvbSFhY2NlbC5oeXBpeGVsLm5ldA==" is a base64 String means "idc.cubepaas.com!accel.hypixel.net"  
+before ! is "from",after is "target"  
--- src/main/java/org/meditation/DNSCacheMod/DNSModifyCommand.java
+++ src/main/java/org/meditation/DNSCacheMod/DNSModifyCommand.java
@@ -8,6 +8,12 @@ import net.minecraft.util.EnumChatFormatting;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.meditation.DNSCacheMod.utils.FileUtils;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.util.Arrays;
+import java.util.Base64;
+
 public class DNSModifyCommand extends CommandBase {
 
     @Override
@@ -22,7 +28,13 @@ public class DNSModifyCommand extends CommandBase {
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return EnumChatFormatting.GREEN+"DNSCacheMod by MeditationDev!\n"+EnumChatFormatting.WHITE+"/dnscachemod add <from> <target>"+EnumChatFormatting.GRAY+" ADD A DOMAIN FOR DNSCACHE\n"+EnumChatFormatting.WHITE+"/dnscachemod remove <from>"+EnumChatFormatting.GRAY+" REMOVE A DOMAIN FOR DNSCACHE\n"+EnumChatFormatting.WHITE+"/dnscachemod list"+EnumChatFormatting.GRAY+" LIST DOMAINS USING DNSCACHE\n"+EnumChatFormatting.WHITE+"/dnscachemod reload"+EnumChatFormatting.GRAY+" RELOAD DNSCACHES";
+        return EnumChatFormatting.GREEN+"DNSCacheMod by MeditationDev!\n"
+                +EnumChatFormatting.WHITE +"/dnscachemod add <from> <target>"+EnumChatFormatting.GRAY+" ADD A DOMAIN FOR DNSCACHE\n"
+                +EnumChatFormatting.WHITE+"/dnscachemod remove <from>"+EnumChatFormatting.GRAY+" REMOVE A DOMAIN FOR DNSCACHE\n"
+                +EnumChatFormatting.WHITE +"/dnscachemod createsharelink <from> <target>"+EnumChatFormatting.GRAY+" CREATE A DNSCACHE SHARE LINK\n"
+                +EnumChatFormatting.WHITE +"/dnscachemod loadsharelink <link>"+EnumChatFormatting.GRAY+" LOAD A DNSCACHE SHARE LINK\n"
+                +EnumChatFormatting.WHITE+"/dnscachemod list"+EnumChatFormatting.GRAY+" LIST DOMAINS USING DNSCACHE\n"
+                +EnumChatFormatting.WHITE+"/dnscachemod reload"+EnumChatFormatting.GRAY+" RELOAD DNSCACHES";
     }
 
     public void sendCommandUsage(ICommandSender sender){
@@ -36,7 +48,7 @@ public class DNSModifyCommand extends CommandBase {
                 sendCommandUsage(sender);
                 return;
             }
-            switch (args[0]){
+            switch (args[0].toLowerCase()){
                 case "add": {
                     if(args.length!=3){
                         sendCommandUsage(sender);
@@ -86,6 +98,44 @@ public class DNSModifyCommand extends CommandBase {
                         String fromIp = java.net.InetAddress.getByName(data.from).getHostAddress();
                         sender.addChatMessage(new ChatComponentText(EnumChatFormatting.WHITE + "FROM:" + data.from + " FROMIP:" + fromIp + " TARGET:" + data.target));
                     }
+                    break;
+                }
+                case "createsharelink":{
+                    if(args.length!=3){
+                        sendCommandUsage(sender);
+                        return;
+                    }
+                    String shareLink="dnscache://",oriShareLink;
+                    oriShareLink=args[1]+"!"+args[2];
+                    Base64.Encoder encoder = Base64.getEncoder();
+                    shareLink+=encoder.encodeToString(oriShareLink.getBytes());
+                    sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW+"Share link is:"+EnumChatFormatting.WHITE+shareLink));
+                    sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN+"Successfully copy to clipboard!"));
+                    StringSelection selection = new StringSelection(shareLink);
+                    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                    clipboard.setContents(selection, selection);
+                    break;
+                }
+                case "loadsharelink":{
+                    if(args.length!=2){
+                        sendCommandUsage(sender);
+                        return;
+                    }
+                    String base64ShareLink,oriShareLink;
+                    String[] splitShareLink;
+                    Base64.Decoder decoder = Base64.getDecoder();
+                    if(args[1].length()<13){
+                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED+"INVALID SHARE LINK"));
+                    }
+                    if(!args[1].substring(0,11).equals("dnscache://")){
+                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED+"INVALID SHARE LINK"));
+                    }
+                    base64ShareLink=args[1].substring(11);
+                    oriShareLink=new String(decoder.decode(base64ShareLink));
+                    splitShareLink=oriShareLink.split("!");
+                    String[] cmdArgs={"add",splitShareLink[0],splitShareLink[1]};
+                    sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW+"ADDING SHARE LINK:\n"+EnumChatFormatting.WHITE+"FROM:"+splitShareLink[0]+" TARGET:"+splitShareLink[1]));
+                    this.processCommand(sender,cmdArgs);
                     break;
                 }
                 case "reload": {
